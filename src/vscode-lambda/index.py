aba69d88ed2e532318c792cb5605e3edc7a9a708
Fabrizio Siciliano <fa.siciliano@reply.de>
2022-09-05 14:33:36 +0200

    improved vscode lambda function

--- src/vscode-lambda/index.py
+++ src/vscode-lambda/index.py
@@ -5,15 +5,16 @@ import os
 def handler(event, context):
     client = boto3.client('cloudformation')
     stack_id = os.environ['VSCODE_STACK_ID']
+    server_port = os.environ['VSCODE_SERVER_PORT']
 
     described_stacks = client.describe_stacks(StackName=stack_id)
     for s in described_stacks["Stacks"]:
-        if s["StackName"] == stack_id:
+        if s["StackId"] == stack_id:
             for o in s["Outputs"]:
                 if "LoadBalancerDNS" in o["OutputKey"]:
                     return {
                         'statusCode': 302,
-                        'headers': {'Location': o["OutputValue"]}
+                        'headers': {'Location': "{base_url}:{port}".format(base_url=o["OutputValue"], port=server_port)}
                     }
 
     return {
